102d64bbb8cff0ce52904468b22edd5970ec674d
Victor Ung <vung@ucsd.edu>
2021-02-17 21:42:18 -0800

    updated template

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
-
 /**
  * Module dependencies.
  */
+const createError = require("http-errors");
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const methodOverride = require("method-override");
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var handlebars = require('express3-handlebars')
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var index = require('./routes/index');
-// Example route
-// var user = require('./routes/user');
-
-var app = express();
+const app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// app.use(express.favicon());
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('IxD secret key'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(cookieParser());
+// app.use(express.session());
+// app.use(app.router);
+app.use(express.static(path.join(__dirname, "public")));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-app.get('/', index.view);
-// Example route
-// app.get('/users', user.list);
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+http.createServer(app).listen(app.get("port"), function () {
+    console.log("Express server listening on port " + app.get("port"));
 });
